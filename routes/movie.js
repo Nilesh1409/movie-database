7ef60774fa88272e5e0e6a8e2430fd63e5688ce8
Nilesh1409 <nileshtiwari7054@gmail.com>
2022-07-17 21:15:50 +0530

    added pagination

--- routes/movie.js
+++ routes/movie.js
@@ -4,7 +4,7 @@ const Movie = require('../database/movies');
 const routeHandler = express.Router();
 
 
-async function getAllMovie (req,res,next){
+async function getAllMovie (req,res){
     let title = req.query.title;
     let rating = req.query.rating;
     if(title !== undefined && rating !== undefined){
@@ -36,7 +36,7 @@ async function getAllMovie (req,res,next){
     })
 }
 
-async function addMovie(req,res,next){
+async function addMovie(req,res){
     let data = req.body;
     console.log(data);
       
@@ -44,8 +44,23 @@ async function addMovie(req,res,next){
        return res.send(data);
 }
 
+async function pagenated(req,res){
+    const {page , per_page} = req.query;
+    if(page){
+    let data = await Movie.find().skip((page-1)*per_page).limit(per_page);
+    if(data.length!==0){
+    return res.send(data);
+    }
+    return res.send({error : 'Page does not exist'})
+    }
+    return res.send({error : 'Something went wrong'})
+    
+}
+
 
 routeHandler.post('/',addMovie)
-routeHandler.get('/',getAllMovie)
+routeHandler.get('/all',getAllMovie)
+routeHandler.get('/',pagenated)
+
 
 module.exports = routeHandler;
